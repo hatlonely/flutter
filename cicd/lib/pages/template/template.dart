1fe89aa2a1f64d48d3ab670280603243801cc784
hatlonely.hl <hatlonely.hl@alibaba-inc.com>
2020-11-22 23:40:16 +0800

    update

--- cicd/lib/pages/template/template.dart
+++ cicd/lib/pages/template/template.dart
@@ -1,8 +1,9 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
-import 'template_view.dart';
-import 'list_tempalte_view.dart';
+import 'package:cicd/pages/template/template_view.dart';
+import 'package:cicd/api/cicd.pb.dart' as api;
+import 'package:cicd/pages/template/put_template_view.dart';
 
 class TemplatePage extends StatefulWidget {
   @override
@@ -17,6 +18,82 @@ class TemplatePageState extends State<TemplatePage> {
       body: Center(
         child: ListTemplateView(),
       ),
+    );
+  }
+}
+
+class ListTemplateView extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => ListTemplateViewState();
+}
+
+class ListTemplateViewState extends State<ListTemplateView> {
+  Future<api.ListTemplateRes> listTemplate() async {
+    var httpClient = http.Client();
+    var res = await httpClient.get("http://127.0.0.1/v1/listTemplate?offset=0&limit=20");
+
+    var listTemplateRes = api.ListTemplateRes();
+    listTemplateRes.mergeFromProto3Json(json.decode(res.body));
+
+    return listTemplateRes;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var res = listTemplate();
+    print(res.then((value) => {print(value)}));
+
+    return FutureBuilder(
+      initialData: api.ListTemplateRes(),
+      future: listTemplate(),
+      builder: (context, value) {
+        var res = value.data as api.ListTemplateRes;
+        var cards = <Widget>[];
+        for (var tpl in res.templates) {
+          cards.add(GestureDetector(
+            onTap: () =>
+                {Navigator.push(context, MaterialPageRoute(builder: (context) => TemplateViewPage(id: tpl.id)))},
+            child: Card(
+              margin: EdgeInsets.symmetric(vertical: 15, horizontal: 15),
+              clipBehavior: Clip.antiAlias,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(8.0),
+              ),
+              elevation: 2,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(tpl.name),
+                  Text(tpl.description),
+                ],
+              ),
+            ),
+          ));
+        }
+
+        cards.add(GestureDetector(
+            onTap: () => {Navigator.push(context, MaterialPageRoute(builder: (context) => PutTemplateViewPage()))},
+            child: Card(
+                margin: EdgeInsets.symmetric(vertical: 15, horizontal: 15),
+                clipBehavior: Clip.antiAlias,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(8.0),
+                ),
+                elevation: 2,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(Icons.add, size: 60),
+                  ],
+                ))));
+
+        return GridView.count(
+          padding: EdgeInsets.symmetric(vertical: 20, horizontal: 40),
+          crossAxisCount: 3,
+          childAspectRatio: 1.618,
+          children: cards,
+        );
+      },
     );
   }
 }
