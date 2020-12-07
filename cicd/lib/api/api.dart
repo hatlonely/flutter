def2d1850316ceaa5330ac447f19a82040854516
hatlonely <hatlonely@gmail.com>
2020-12-07 23:26:00 +0800

    update

--- cicd/lib/api/api.dart
+++ cicd/lib/api/api.dart
@@ -1,8 +1,7 @@
-library swagger.api;
+library openapi.api;
 
 import 'dart:async';
 import 'dart:convert';
-import 'package:http/browser_client.dart';
 import 'package:http/http.dart';
 
 part 'api_client.dart';
@@ -12,10 +11,14 @@ part 'auth/authentication.dart';
 part 'auth/api_key_auth.dart';
 part 'auth/oauth.dart';
 part 'auth/http_basic_auth.dart';
+part 'auth/http_bearer_auth.dart';
 
 part 'api/cicd_service_api.dart';
 
-part 'model/api_empty.dart';
+part 'model/api_del_job_res.dart';
+part 'model/api_del_task_res.dart';
+part 'model/api_del_template_res.dart';
+part 'model/api_del_variable_res.dart';
 part 'model/api_get_templates_req.dart';
 part 'model/api_get_variables_req.dart';
 part 'model/api_job.dart';
@@ -23,10 +26,16 @@ part 'model/api_list_job_res.dart';
 part 'model/api_list_task_res.dart';
 part 'model/api_list_template_res.dart';
 part 'model/api_list_variable_res.dart';
+part 'model/api_put_task_res.dart';
+part 'model/api_put_template_res.dart';
+part 'model/api_put_variable_res.dart';
 part 'model/api_run_task_req.dart';
 part 'model/api_run_task_res.dart';
 part 'model/api_task.dart';
 part 'model/api_template.dart';
+part 'model/api_update_task_res.dart';
+part 'model/api_update_template_res.dart';
+part 'model/api_update_variable_res.dart';
 part 'model/api_variable.dart';
 part 'model/job_sub.dart';
 part 'model/protobuf_any.dart';
@@ -34,4 +43,4 @@ part 'model/runtime_error.dart';
 part 'model/template_script_template.dart';
 
 
-ApiClient defaultApiClient = new ApiClient();
+ApiClient defaultApiClient = ApiClient();
